658964778fae352ce33281e2b1380862ddff1043
Nick Galluzzo <n.galluzzo@gmail.com>
2020-02-15 14:30:24 -0500

    add user_admin controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users_admin, controller: :users
+
   root to: 'pages#home'
   resources :articles, only: [:index, :new, :create, :show, :edit, :destroy, :update ]
 end
