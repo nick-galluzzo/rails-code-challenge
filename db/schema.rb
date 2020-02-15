402a148f40c53d00ae16fd33a49c50481e920974
Nick Galluzzo <n.galluzzo@gmail.com>
2020-02-15 09:58:19 -0500

    add articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_194805) do
+ActiveRecord::Schema.define(version: 2020_02_14_221633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,11 +18,18 @@ ActiveRecord::Schema.define(version: 2020_02_14_194805) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.string "category"
+    t.bigint "category_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,5 +44,6 @@ ActiveRecord::Schema.define(version: 2020_02_14_194805) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "categories"
   add_foreign_key "articles", "users"
 end
