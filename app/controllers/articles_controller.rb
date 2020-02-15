9f5bf256e4c59a96057640dd20adb6cdb626d94e
Nick Galluzzo <n.galluzzo@gmail.com>
2020-02-15 09:55:23 -0500

    add pundit authentication to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,32 @@
 class ArticlesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
+
   def index
+    @articles = Article.where(category: params[:category])
+    @category = Category.find(params[:category])
+    authorize @articles
   end
 
   def show
+    @article = Article.find(params[:id])
+    authorize @article
   end
 
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
+  end
 end
